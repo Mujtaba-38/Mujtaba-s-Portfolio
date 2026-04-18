b4ae909d249d31971b28b624a2cafa6a61aebce6
Mujtaba-38 <69086@cch.edu.pk>
2026-04-18 15:53:48 +0500

    Update bubblesort1o1.py

--- bubblesort1o1.py
+++ bubblesort1o1.py
@@ -7,24 +7,22 @@ print(x)
 
 print("today we are going to build a stack for the first time. ")
 
-TheStack= [1,3,5,7,8,45,23,56,78,98,87,233,546,675]
-Pass = 1
-Swap = False
-while Pass < len(TheStack)-1 and Swap == False:
-    for index in range(1, len(TheStack)-1):
-     if TheStack[index] > TheStack[index+1]:
-        temp = TheStack[index]
-        TheStack[index]= TheStack[index+1]
-        TheStack[index+1] = temp
-        Swap = True
-    Pass = Pass +1
+TheStack = [1, 3, 5, 7, 8, 45, 23, 56, 78, 98, 87, 233, 546, 675]
+Pass = 0
+n = len(TheStack)
 
-for i in range(1, len(TheStack)):
-   print(TheStack[i])
+for i in range(n):
+    swapped = False
+    for j in range(0, n - i - 1):
+        if TheStack[j] > TheStack[j + 1]:
+            # Swap elements
+            temp = TheStack[j]
+            TheStack[j] = TheStack[j + 1]
+            TheStack[j + 1] = temp
+            swapped = True
+    if not swapped:
+        break
 
-
-
-
- 
-
-
+print("\nSorted array:")
+for i in range(len(TheStack)):
+    print(TheStack[i])
